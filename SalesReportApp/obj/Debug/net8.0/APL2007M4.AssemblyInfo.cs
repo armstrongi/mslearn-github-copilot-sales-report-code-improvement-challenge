6b9e654dbf5d832355bf0388ef618177c709898b
Ian Armstrong <50608249+armstrongi@users.noreply.github.com>
2025-02-13 03:54:34 +0000

    Modularise the functions reated to QuarterlySalesDataByDepartment v2

--- SalesReportApp/obj/Debug/net8.0/APL2007M4.AssemblyInfo.cs
+++ SalesReportApp/obj/Debug/net8.0/APL2007M4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APL2007M4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02191f484d5eae45544b70e74df5c51c770906d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ac7244031ecbfa6c52accafd0c3b8bed429e166")]
 [assembly: System.Reflection.AssemblyProductAttribute("APL2007M4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APL2007M4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
